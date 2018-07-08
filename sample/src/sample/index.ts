3e685d444e97c0e2b9d8fc4ebc0819ac307d256d
Efremenko Roman <Roman.Efremenko@infowatch.com>
2018-07-08 17:36:42 +0300

    + add schema to command;

--- sample/src/sample/index.ts
+++ sample/src/sample/index.ts
@@ -1,9 +1,10 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
+import { Schema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function sample(options: any): Rule {
+export function sample(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const name = options.name || 'Unknown';
 
